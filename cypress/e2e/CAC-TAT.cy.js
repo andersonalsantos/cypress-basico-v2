ded77214f2ac05824d58167d1f6ed2cd72753dab
Anderson Almeida <anderson.almeida@garupa.co>
2025-01-10 13:21:19 -0300

    ajustando caminho

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -2,7 +2,7 @@
 
 describe('Central de Atendimento ao Cliente TAT', function () {
   beforeEach(function () {
-    cy.visit('./src/index.html')
+    cy.visit('/')
   })
 
   it('verifica o título da aplicação', function () {
