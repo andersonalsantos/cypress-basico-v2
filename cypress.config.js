7c25927ec392a403906391a297d472c28d8b08e2
Anderson Almeida <anderson.almeida@garupa.co>
2025-01-10 12:50:37 -0300

    Tentando github

--- cypress.config.js
+++ cypress.config.js
@@ -1,39 +1,11 @@
 module.exports = {
   e2e: {
-    setupNodeEvents(on, config) {
-      // Aqui você pode adicionar eventos específicos para testes E2E
-      // Exemplo de configuração personalizada de eventos:
-      on('before:browser:launch', (browser = {}, launchOptions) => {
-        // Configurações para iniciar o navegador
-        return launchOptions;
-      });
-    },
-    
+    // Configurações do Cypress (exemplo)
+    baseUrl: 'http://localhost:3000',
     viewportWidth: 1280,
-    viewportHeight: 880,
-    //ativando a captura em video dos testes
-   // video: true,
-    // Outras configurações específicas para testes E2E
-  },
-
-  component: {
+    viewportHeight: 720,
     setupNodeEvents(on, config) {
-      // Aqui você pode adicionar eventos específicos para testes de componentes
-      // Exemplo: alterando a configuração do bundler para os testes de componentes
+      // Você pode adicionar eventos aqui, como plugins
     },
-    // Outras configurações específicas para testes de componentes
   },
 };
-
-const { defineConfig } = require('cypress');
-
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'http://localhost:3000', // Atualize conforme necessário
-    setupNodeEvents(on, config) {
-      // Adicione aqui eventos necessários
-    },
-    specPattern: 'cypress/e2e/**/*.cy.{js,ts}', // Caminho para seus testes
-  },
-});
-
