1c743f5bde6cf60737dd78cc4558b44dd455e0d2
Anderson Almeida <anderson.almeida@garupa.co>
2025-01-10 17:12:29 -0300

    ajusta de repetição

--- cypress/e2e/pricacyPolicy.cy.js
+++ cypress/e2e/pricacyPolicy.cy.js
@@ -1,7 +1,10 @@
-it.only('verifica o título da aplicação', function () {
+Cypress._.times(3,() => { //comando para repetição de teste
+it('verifica o título da aplicação', function () {
   // cy.visit('./src/privacy.html')
   cy.visit('/privacy.html')
 
   cy.contains('h1', 'CAC TAT - Política de privacidade' ).should('be.visible')
+
+})
 
 })
