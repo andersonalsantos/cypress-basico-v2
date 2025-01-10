2e26547a5c7bb6c2c7645e17a8cf5524b904a8ce
Anderson Almeida <anderson.almeida@garupa.co>
2025-01-10 12:56:24 -0300

    Nova Tentantiva

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   e2e: {
-    // Configurações do Cypress (exemplo)
     baseUrl: 'http://localhost:3000',
     viewportWidth: 1280,
     viewportHeight: 720,
     setupNodeEvents(on, config) {
-      // Você pode adicionar eventos aqui, como plugins
+      // Adicione plugins ou eventos aqui se necessário
     },
   },
 };
